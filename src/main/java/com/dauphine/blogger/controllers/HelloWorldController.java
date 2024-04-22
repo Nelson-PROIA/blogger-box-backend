b427363a6b0da6bea582e960f4cf72c341811e2f
Nelson PROIA <nelson.proia@dauphine.eu>
2024-04-22 15:07:14 +0200

    Add documentation via Swagger (bis)

--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -6,22 +6,32 @@
  */
 package com.dauphine.blogger.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(
+        name = "Hello world API",
+        description = "My first 'Hello world' endpoints"
+)
 public class HelloWorldController {
 
     /**
-     * Returns a simple "Hello World!" message.
+     * Returns a simple 'Hello World!' message.
      *
      * @return "Hello World!" message
      */
     @GetMapping("hello-world")
+    @Operation(
+            summary = "Hello world endpoint",
+            description = "Returns 'Hello world!'"
+    )
     public String helloWorld() {
-        return "Hello World!";
+        return "Hello world!";
     }
 
     /**
@@ -31,6 +41,10 @@ public class HelloWorldController {
      * @return personalized greeting message
      */
     @GetMapping("hello")
+    @Operation(
+            summary = "Hello by name endpoint (from request param)",
+            description = "Returns 'Hello {name}' from request param"
+    )
     public String helloFromRequestParam(@RequestParam String name) {
         return "Hello " + name + " (from request param)!";
     }
@@ -42,8 +56,12 @@ public class HelloWorldController {
      * @return personalized greeting message
      */
     @GetMapping("hello/{name}")
+    @Operation(
+            summary = "Hello by name endpoint (by path variable)",
+            description = "Returns 'Hello {name}' by path variable"
+    )
     public String helloFromPathVariable(@PathVariable String name) {
-        return "Hello " + name + " (from path variable)!";
+        return "Hello " + name + " (by path variable)!";
     }
 
 }
